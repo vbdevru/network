3f7c69544930d2a73331595b429835cd85664984
vbdevru <vbdevru@mail.ru>
2022-07-14 07:13:16 +0300

    Get request

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(Home());
+  runApp(const NetworkApp());
 }
 
-class Home extends StatelessWidget {
+class NetworkApp extends StatelessWidget {
+  const NetworkApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Text('Network'),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Networking',
+      home: Home(),
     );
   }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Networking'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+Future<http.Response> getData() async {
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  return await http.get(Uri.parse(url));
+}
+
+void loadData() {
+  getData().then((value) {
+    if (value.statusCode == 200) {
+      print(value.body);
+    } else {
+      print(value.statusCode);
+    }
+  }).catchError((error) {
+    debugPrint(error.toString());
+  });
 }
